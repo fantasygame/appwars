123682b486be290b261283bfa4f272a6d0285aff
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-09 01:31:57 +0100

    Update validation and relation specs

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
+  it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:duration_in_seconds) }
-  it { is_expected.to belong_to(:user) }
 end
--- spec/models/war_spec.rb
+++ spec/models/war_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe War, type: :model do
+  it { is_expected.to have_one(:review) }
+  it { is_expected.to validate_presence_of(:review) }
 end
